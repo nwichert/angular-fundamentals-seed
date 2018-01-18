0ace01e4c288783817a6ffd4537ffcac119d4968
Nick Wichert <nick.wichert@icloud.com>
2018-01-17 22:51:00 -0600

    finalized Rendering Flows section

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,72 @@
 import { Component } from '@angular/core';
 
+interface Child {
+  name: string,
+  age: number
+}
+
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean,
+  checkedInDate: number | null,
+  children: Child[] | null
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span 
+            class="status"
+            [class.checked-in]="passenger.checkedIn"></span>
+          {{ i }}: {{ passenger.fullname }}
+          <div class="date">
+            Check in date: 
+              {{ passenger.checkedInDate ? (passenger.checkedInDate | date: 'yMMMMd') : 'Not checked in' }}
+          </div>
+          <div class="children">
+            Children: {{ passenger.children?.length || 'N/A' }}
+          </div>
+        </li>
+      </ul>
     </div>
   `
 })
 export class AppComponent {
-
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephan',
+    checkedIn: true,
+    checkedInDate: 1490742000000,
+    children: null
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: true,
+    checkedInDate: 1490742000000,
+    children: null
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true,
+    checkedInDate: 1488412800000,
+    children: null
+  }, {
+    id: 4,
+    fullname: 'Nick',
+    checkedIn: false,
+    checkedInDate: null,
+    children: [{ name: 'Addie', age: 6},{ name: 'Belle', age: 4}, {name: 'William', age: 1}]
+  }, {
+    id: 5,
+    fullname: 'Jackie',
+    checkedIn: false,
+    checkedInDate: null,
+     children: [{ name: 'Addie', age: 6},{ name: 'Belle', age: 4}, {name: 'William', age: 1}]
+  }];
 }
